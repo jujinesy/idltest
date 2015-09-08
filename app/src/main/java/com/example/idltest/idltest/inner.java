5a98f7e0ff72fb500152a135441262fec3b8326a
jujin bae <jujinesy@gmail.com>
2015-09-09 03:26:13 +0900

    inner

--- app/src/main/java/com/example/idltest/idltest/inner.java
+++ app/src/main/java/com/example/idltest/idltest/inner.java
@@ -1,25 +1,22 @@
 package com.example.idltest.idltest;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class inner extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inner);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_inner, menu);
         return true;
     }
 
@@ -36,12 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onstartbtnClicked(View view)
-    {
-        Toast.makeText(getApplicationContext(), "rrrrr", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, inner.class);
-        startActivity(intent);
     }
 }
